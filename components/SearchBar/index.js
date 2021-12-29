bb31bbfcfcf7cebf4887c1420544d2cfcbdb0fe1
Hayden M <haydenmlh@gmail.com>
2021-12-29 01:30:07 -0500

    add dotenv plugin

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import {View, Text, TextInput, TouchableOpacity, Image, Dimensions} from 'react-native';
 import styles from './styles';
+import {API_KEY} from '@env';
 
 const SearchBar = (props) => {
   
   const onSearch = (text) => {
-    console.warn(text);
+    console.warn(cityOrZipcode(text));
+    let callCity = 'api.openweathermap.org/data/2.5/weather?q='
+                +text
+                +'&appid='
+                + API_KEY;
+    let callZip = 'api.openweathermap.org/data/2.5/weather?zip='
+                  +text
+                  +'&appid='
+                  + API_KEY;
+    cityOrZipcode(text) ? callZip : callCity;
   };
+
+  const callCity = (text) => {
+    let callCity = 'api.openweathermap.org/data/2.5/weather?q='
+                +text
+                +'&appid='
+                + API_KEY;
+  }
+
+  const cityOrZipcode = (text) => {
+    let re = /\d+/;
+    return re.test(text);
+  }
 
   return(
     <View style={styles.searchWrap}>
